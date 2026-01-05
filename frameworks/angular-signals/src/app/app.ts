4d2cd3cfe165b7cb06bb13d60638be1400f163cf
Ostap Korotchenok <o.korotchenok@yadro.com>
2026-01-05 22:16:06 +0300

    - add node attributes

--- frameworks/angular-signals/src/app/app.ts
+++ frameworks/angular-signals/src/app/app.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { TreeNode } from './model'
 import { TreeNodeComponent } from './tree-node'
+import { NodeAttributesComponent } from './node-attributes'
 
 @Component({
   selector: 'app-root',
   template: `
     <tree-node [node]="tree" />
+    <node-attributes />
   `,
   imports: [
     TreeNodeComponent,
+    NodeAttributesComponent,
   ],
 })
 export class App {
   readonly tree = TreeNode.fromRaw(
     {
       title: 'Root Node 1',
+      attributes: [{title: 'attr1', value: '10'}],
       children: [
         {
           title: 'Child 1.1',
+          attributes: [{title: 'attr1.1', value: '10'}],
           children: [
             {
               title: 'Grandchild 1.1.1',
