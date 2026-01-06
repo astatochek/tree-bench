90541db28e2b3487c5cab65ad820f9fa68cfef0b
Ostap Korotchenok <o.korotchenok@yadro.com>
2026-01-06 10:30:07 +0300

    - add angular pull

---
+++ frameworks/angular-pull/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: "app-root",
+  template: `
+    <h1>Welcome to {{ title() }}!</h1>
+  `,
+  styles: [],
+})
+export class App {
+  protected readonly title = signal("angular-pull");
+}
