20237a4e28e3354abb76e8faf1875865c040e36b
Ostap Korotchenok <o.korotchenok@yadro.com>
2026-01-07 13:16:46 +0300

    - install all script

--- runner/src/sut.ts
+++ runner/src/sut.ts
@@ -7,6 +7,6 @@ export type Context = {
 
 export const sut: Context[] = [
   { title: "Angular Signals", dir: "angular-signals", port: 3000, results: [] },
-  { title: "Angular Pure Pull", dir: "angular-pull", port: 3001, results: [] },
-  { title: "Angular Pure Push", dir: "angular-push", port: 3002, results: [] },
+  { title: "Angular Pull", dir: "angular-pull", port: 3001, results: [] },
+  { title: "Angular Push", dir: "angular-push", port: 3002, results: [] },
 ];
